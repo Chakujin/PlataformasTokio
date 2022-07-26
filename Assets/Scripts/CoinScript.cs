e1021aa2e68ef130d5eae1e11409cd136e0fc83c
Chaku <cristiangalindo13@gmail.com>
2022-07-26 11:39:44 +0200

    Añadiendo sprites

---
+++ Assets/Scripts/CoinScript.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class CoinScript : MonoBehaviour
+{
+    private GameManager m_gameManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_gameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            m_gameManager.coins++;
+        }
+    }
+}
