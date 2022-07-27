ba30b81169f7116570b5e27896b02ace0ed8017a
Chaku <cristiangalindo13@gmail.com>
2022-07-27 18:39:42 +0200

    ScriptableObjects

---
+++ Assets/Scripts/ScriptableObject/LevelData.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "LevelObject")]
+public class LevelData : ScriptableObject
+{
+    //Items Properties
+    public string itemName;
+    public Sprite itemImage;
+    // public Image backgroundSelected;
+}
---
+++ Assets/Scripts/ScriptableObject/LevelObject.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class LevelObject : MonoBehaviour
+{
+    //Data
+    private LevelData m_data;
+
+    public Image image;
+    public TextMeshProUGUI nameText;
+
+    //Data to ItemManager
+    public LevelData Data
+    {
+        set //Take values from data
+        {
+            m_data = value;
+            image.sprite = m_data.itemImage;
+            nameText.text = m_data.itemName;
+        }
+    }
+}
